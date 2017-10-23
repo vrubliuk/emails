c42f818d5f10ddda78dd16cb06275c2fcb586aa6
vrubliuk <vrubliuk@gmail.com>
2017-10-23 15:29:51 +0300

    Started

--- javascript/main-dist.js
+++ javascript/main-dist.js
@@ -2,4 +2,16 @@
 
 (function () {
     "use strict";
+
+    var pendingPickup = "Hi,<br>APlease send the release sheet for \n    Thanks";
+
+    document.getElementById("button").onclick = function () {
+
+        var inp = document.createElement('input');
+        document.body.appendChild(inp);
+        inp.value = pendingPickup;
+        inp.select();
+        document.execCommand('copy');
+        inp.remove();
+    };
 })();
--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,17 @@
 ﻿(function(){
     "use strict";
+    const pendingPickup = `Hi,<br>APlease send the release sheet for 
+    Thanks`;
+
+    document.getElementById("button").onclick = () => {
     
+        var inp = document.createElement('input');
+        document.body.appendChild(inp);
+        inp.value = pendingPickup;
+        inp.select();
+        document.execCommand('copy');
+        inp.remove();
+    };
 
 
 
